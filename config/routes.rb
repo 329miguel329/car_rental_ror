49260eea2a5bc9cce76e90983292b07768ee341b
Miguel Muñoz <miguel329924@gmail.com>
2020-01-13 04:47:29 -0500

    [UPD] Add Dashboard and update sidenav and headnav.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "card_types#index"
+  get 'dashboard/index'
+  root "dashboard#index"
 
   devise_for :users, :controllers => { registrations: 'users/registrations'}
 
