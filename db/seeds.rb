d2b90b2d30588f510e222a52e9bc0e5087f0a157
Miguel Muñoz <miguel329924@gmail.com>
2020-01-12 21:38:28 -0500

    [ADD] Add code for create data in models CardType and BloodType.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# CardType
+CardType.create({:code => "C.C.", :name => "Cédula de Ciudadanía"})
+CardType.create({:code => "C.E.", :name => "Cédula de Extranjería"})
+CardType.create({:code => "P.", :name => "Pasaporte"})
+
+# BloodType
+BloodType.create({:code => 'O+', :name => 'O Positivo'})
+BloodType.create({:code => 'O-', :name => 'O Negativo'})
+BloodType.create({:code => 'AB+', :name => 'AB+ Positivo'})
+
