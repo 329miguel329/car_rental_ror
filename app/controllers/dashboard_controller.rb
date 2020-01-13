578dd8c54e1bce50393f896e71b23914a17a971e
Miguel Muñoz <miguel.munoz@longportaviation.com>
2020-01-13 15:13:25 -0500

    [ADD] Add form of passenger to dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   add_breadcrumb "<i class='fa fa-home'></i> #{I18n.t("gui.menu.home")}".html_safe, :root_path
   def index
+    @user = User.new({:rol => CarRental::ROL_PASSENGER})
+  end
+
+  def create_form
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
+  post 'dashboard/create_form'
   root "dashboard#index"
 
   devise_for :users, :controllers => {
@@ -9,13 +10,13 @@ Rails.application.routes.draw do
 
   resources :blood_types do
     collection do
-      get "select2"
+      get 'select2'
     end
   end
 
   resources :card_types do
     collection do
-      get "select2"
+      get 'select2'
     end
   end
 end
